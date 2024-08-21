d184d87f09cb6070ad921c1c0b7ae817207b78d8
StevenTB1 <stevenchow0812@gmail.com>
2024-08-20 21:37:45 -0400

    8.20

--- react-hook/src/App.jsx
+++ react-hook/src/App.jsx
@@ -2,15 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import MyComponent from './MyComponent'
+import Counter from './Counter.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <MyComponent />
-    </>
+      <Counter />
   )
 }
-
 export default App
