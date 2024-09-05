7109e394450fbaade77364cc93ec37f702283500
StevenTB1 <stevenchow0812@gmail.com>
2024-09-04 22:41:21 -0400

    9.4

---
+++ color-picker/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ColorPicker from './ColorPicker.jsx';
+
+function App() {
+  return (<ColorPicker />);
+}
+
+export default App
