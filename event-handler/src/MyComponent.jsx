4e8bb2b7678e133b0af6241b7ad491eff7813caf
StevenTB1 <stevenchow0812@gmail.com>
2024-09-04 22:03:52 -0400

    9.4

--- event-handler/src/MyComponent.jsx
+++ event-handler/src/MyComponent.jsx
@@ -5,7 +5,7 @@ function MyComponent(){
     const [quantity, setQuantity] = useState(1);
     const [comment, setComment] = useState("");
     const [payment, setPayment] = useState("");
-    const [shipping, setShipping] = useState()
+    const [shipping, setShipping] = useState("Delivery");
 
     function handleNameChange(event){
         setName(event.target.value);
@@ -21,6 +21,10 @@ function MyComponent(){
 
     function handlePaymentChange(event){
         setPayment(event.target.value);
+    }
+
+    function handleShippingChange(event){
+        setShipping(event.target.value);
     }
 
     return (
@@ -43,6 +47,21 @@ function MyComponent(){
         </select>
 
         <p>Payment: {payment}</p>
+
+        <label>
+            <input type="radio" value="Pick Up"
+                    checked={shipping === "Pick Up"}
+                    onChange={handleShippingChange}/>
+            Pick Up
+        </label> <br />
+        <label>
+        <input type="radio" value="Delivery"
+                    checked={shipping === "Delivery"}
+                    onChange={handleShippingChange}/>
+            Delivery
+        </label>
+
+        <p>Shipping: {shipping}</p>
     </div>);
 }
 
