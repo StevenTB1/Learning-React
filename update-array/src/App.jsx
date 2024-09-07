4896947904afc9d2e1969a98fb35064004d23b38
StevenTB1 <stevenchow0812@gmail.com>
2024-09-06 22:56:11 -0400

    9.6

---
+++ update-array/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import MyComponent from './MyComponent'
+
+function App() {
+
+  return (
+    <MyComponent/>
+  )
+}
+
+export default App
