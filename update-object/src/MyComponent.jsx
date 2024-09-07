23e05751c9e3165baf960025eddff0a417ebdec8
StevenTB1 <stevenchow0812@gmail.com>
2024-09-06 20:46:28 -0400

    9.6

--- update-object/src/MyComponent.jsx
+++ update-object/src/MyComponent.jsx
@@ -6,12 +6,24 @@ function MyComponent(){
                                     make: "Tesla", 
                                     model: "Y"});
 
+    function handleYearChange(event){
+        setCar(c => ({...c, year: event.target.value}));
+    }
+
+    function handleMakeChange(event){
+        setCar(c =>({...c, make: event.target.value}));
+    }
+
+    function handleModelChange(event){
+        setCar(c =>({...c, model: event.target.value}));
+    }
+
     return (<div>
         <p>Your favourite car is: {car.year} {car.make} {car.model}</p>
 
-        <input type="number" value={car.year}/> <br />
-        <input type="text" value={car.make}/> <br />
-        <input type="text" value={car.model}/> <br />
+        <input type="number" value={car.year} onChange={handleYearChange}/> <br />
+        <input type="text" value={car.make} onChange={handleMakeChange}/> <br />
+        <input type="text" value={car.model} onChange={handleModelChange}/> <br />
         </div>
     );
 }
