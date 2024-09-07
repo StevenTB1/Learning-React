5b14ecd43cd74288fdbbc1e7c2bdd220b0dec557
StevenTB1 <stevenchow0812@gmail.com>
2024-09-06 23:51:52 -0400

    9.6

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ToDoList from './ToDoList'
+
+function App() {
+
+  return (
+    <ToDoList/>
+  )
+}
+
+export default App
