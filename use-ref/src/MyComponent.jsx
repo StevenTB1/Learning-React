9fbd57709b44f08ffaea947b1a957ab4d0acd358
StevenTB1 <stevenchow0812@gmail.com>
2024-09-07 23:11:52 -0400

    9.7

---
+++ use-ref/src/MyComponent.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+function MyComponent(){
+
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    useEffect(() => {
+        console.log("Component rendered");
+    })
+
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "yellow";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef2.current.style.backgroundColor = "yellow";
+        inputRef1.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef3.current.style.backgroundColor = "yellow";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef1.current.style.backgroundColor = "";
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick1}>
+                Click Me 1!
+            </button>
+            <input ref={inputRef1}/>
+
+            <button onClick={handleClick2}>
+                Click Me 2!
+            </button>
+            <input ref={inputRef2}/>
+
+            <button onClick={handleClick3}>
+                Click Me 3!
+            </button>
+            <input ref={inputRef3}/>
+        </div>
+    );
+}
+
+export default MyComponent;
